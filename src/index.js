1f06590720996658b44153c61aa36e975eb1b1f7
tandav21 <tvdavyd@gmail.com>
2021-03-15 12:54:58 +0300

    feat: add js to return empty array

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if (matrix == undefined || matrix === null || matrix.length == 0) {
   return [];
+  }
+  
 }
